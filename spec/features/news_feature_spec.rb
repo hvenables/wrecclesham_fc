73b5c3ef8bc37fa188e071d39b061578046b720b
Harry Venables <venablesh@yahoo.co.uk>
2016-05-08 00:54:18 +0100

    Added tests for the teams helper

--- spec/features/news_feature_spec.rb
+++ spec/features/news_feature_spec.rb
@@ -2,10 +2,20 @@ require 'rails_helper'
 
 feature 'news' do
   context 'no news has been added' do
+    let(:admin) { create :admin }
+
     scenario 'should display that their is no news' do
       visit root_path
       click_link 'News'
       expect(page).to have_css "div.news", text: 'No news has been add yet'
+      expect(page).to_not have_link 'Add News'
+    end
+
+    scenario 'should display add news if admin' do
+      sign_in(admin)
+      click_link 'News'
+      expect(page).to have_css "div.news", text: 'No news has been add yet'
+      expect(page).to have_link 'Add news'
     end
   end
 end
---
+++ spec/helpers/teams_helper_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+describe TeamsHelper do
+  let!(:league_table) { create :league_table }
+  context '#create_teams' do
+    scenario 'will create a team in the db' do
+      table_data = [["UFC Farnham", "18", "13", "2", "3", "67", "31", "+36    ", "41"]]
+      create_teams(table_data, league_table.id)
+      ufc_farnham = Team.first
+      expect(ufc_farnham.team).to eq 'UFC Farnham'
+      expect(ufc_farnham.games).to eq 18
+      expect(ufc_farnham.won).to eq 13
+      expect(ufc_farnham.drawn).to eq 2
+      expect(ufc_farnham.lost).to eq 3
+      expect(ufc_farnham.goals_for).to eq 67
+      expect(ufc_farnham.goals_against).to eq 31
+      expect(ufc_farnham.goal_difference).to eq 36
+      expect(ufc_farnham.points).to eq 41
+      expect(ufc_farnham.league_table_id).to eq league_table.id
+    end
+  end
+  context '#update_teams' do
+    let!(:farnham) { create :team, team: 'UFC Farnham' }
+    scenario 'will update a team in the db' do
+      table_data = [["UFC Farnham", "18", "13", "2", "3", "67", "31", "+36    ", "41"]]
+      binding.pry
+      update_teams(table_data, league_table.id)
+      ufc_farnham = Team.first
+      binding.pry
+      expect(ufc_farnham.team).to eq 'UFC Farnham'
+      expect(ufc_farnham.games).to eq 18
+      expect(ufc_farnham.won).to eq 13
+      expect(ufc_farnham.drawn).to eq 2
+      expect(ufc_farnham.lost).to eq 3
+      expect(ufc_farnham.goals_for).to eq 67
+      expect(ufc_farnham.goals_against).to eq 31
+      expect(ufc_farnham.goal_difference).to eq 36
+      expect(ufc_farnham.points).to eq 41
+      expect(ufc_farnham.league_table_id).to eq league_table.id
+    end
+  end
+end
