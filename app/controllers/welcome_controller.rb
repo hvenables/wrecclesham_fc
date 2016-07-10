3e389f9dbfbe84b168f800a6b38e817b13d8af08
Harry Venables <venablesh@yahoo.co.uk>
2016-07-10 21:51:20 +0100

    Added the reserve team to the home page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,22 +4,31 @@ class WelcomeController < ApplicationController
 
   def index
     if @first_team_table
-      @teams = @first_team_table.teams.ordered_on_points
+      @first_team_teams = @first_team_table.teams.ordered_on_points
       @years = show_year(@first_team_table.year)
-      @teams = teams_around_us(@teams)
+      @first_team_teams = teams_around_us(@first_team_teams, 'UFC Farnham')
+    end
+    if @reserve_team_table
+      @reserve_team_teams = @reserve_team_table.teams.ordered_on_points
+      @reserve_team_teams = teams_around_us(@reserve_team_teams, 'UFC Farnham Reserves')
     end
     @news = News.all
   end
 
   private
 
-  def teams_around_us(all_teams)
-    first_team = Team.find_by(team: 'UFC Farnham')
-    index = all_teams.to_a.index(first_team)
+  def teams_around_us(all_teams, team)
+    team = Team.find_by(team: team)
+    index = all_teams.to_a.index(team)
     numbers = []
     while numbers.length < 7
-      calc_up_and_down(index, numbers) if numbers.length == 0
-      numbers << numbers.last + 1
+      calc_up_and_down(index, numbers, all_teams.length) if numbers.length == 0
+      if numbers.last < all_teams.length
+        numbers << numbers.last + 1
+      else
+        numbers << numbers.first - 1
+        numbers.sort!
+      end
     end
     teams = []
     numbers.each do |number|
@@ -28,7 +37,7 @@ class WelcomeController < ApplicationController
     teams
   end
 
-  def calc_up_and_down(index, numbers)
+  def calc_up_and_down(index, numbers, league_size)
     counter = index
     3.times do
       counter -= 1
@@ -37,7 +46,7 @@ class WelcomeController < ApplicationController
     counter = index
     3.times do
       counter += 1
-      numbers << counter if counter < @teams.length
+      numbers << counter if counter < league_size
     end
     numbers << index
     numbers.sort!
