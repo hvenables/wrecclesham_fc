ef9ed4c815665a286eb4adaa3ded0ccd5e18201d
Harry Venables <venablesh@yahoo.co.uk>
2016-09-21 23:03:35 +0100

    Added validation for news and videos

--- app/models/news.rb
+++ app/models/news.rb
@@ -2,8 +2,7 @@ class News < ApplicationRecord
   has_attached_file :image, :styles => { :large => "600x600>", :medium => "320x250>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
-  validates :title, presence: true
-  validates :summary, presence: true
+  validates :title, :summary, :image, presence: true
   validates :summary, length: { maximum: 250 }
   validates :content, presence: true
 
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,8 @@
 class Video < ApplicationRecord
-
   before_save :add_thumbnail
+
+  validates :title, :summary, :url, presence: :true
+  validate :embedable_url
 
   def url_santizer
     self.url.gsub(/^http.?:/im, "") if self.url[0..3] == 'http'
@@ -9,6 +11,12 @@ class Video < ApplicationRecord
   def add_thumbnail
     video = Yt::Video.new url: self.url
     self.thumbnail = video.thumbnail_url('medium')
+  end
+
+  def embedable_url
+    unless url.include?("embed")
+      errors[:video] << "This is not an embedable link please get the embedable link from youtube"
+    end
   end
 
 end
