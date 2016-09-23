6710ff34a4435400b4a8acdbf746679e1ad4acc2
Harry Venables <harry@tagadab.com>
2016-09-23 12:29:54 +0100

    Add https to images

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,5 +1,8 @@
 class News < ApplicationRecord
-  has_attached_file :image, :styles => { :large => "600x600>", :medium => "320x250>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image,
+    :styles => { :large => "600x600>", :medium => "320x250>", :thumb => "100x100>" },
+    :default_url => "/images/:style/missing.png",
+    :s3_protocol => :https
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   validates :title, :summary, :image, presence: true
