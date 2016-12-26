b88bcd981db597f9f283c3d4cd678fa7c6253947
Harry Venables <venablesh@yahoo.co.uk>
2016-12-26 17:19:06 +0000

    Slight tidy of welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,16 +5,12 @@ class WelcomeController < ApplicationController
     @carousel_articles = latest_carousel_items
     @first_team = Team.first_team
     @reserve_team = Team.reserve_team
-    if @first_team_table
-      @first_team_teams = @first_team.seven_positions_around_team
-    end
-    if @reserve_team_table
-      @reserve_team_teams = @reserve_team.seven_positions_around_team
-    end
+    @first_team_teams = @first_team.seven_positions_around_team if @first_team_table
+    @reserve_team_teams = @reserve_team.seven_positions_around_team if @reserve_team_table
     @first_team_fixture = Fixture.next_game(@first_team)
     @reserve_team_fixture = Fixture.next_game(@reserve_team)
-    @first_team_result= Fixture.last_game(@first_team)
-    @reserve_team_result= Fixture.last_game(@reserve_team)
+    @first_team_result = Fixture.last_game(@first_team)
+    @reserve_team_result = Fixture.last_game(@reserve_team)
     @twitter = TwitterFeed.new
   end
 
@@ -23,7 +19,7 @@ class WelcomeController < ApplicationController
   def latest_carousel_items
     news = News.order(updated_at: :desc).limit(5).to_a
     videos = Video.order(updated_at: :desc).limit(2).to_a
-    carousel_items = news.concat(videos)
+    carousel_items = news + videos
 
     carousel_items.sort! do |first_item, second_item|
       second_item.created_at <=> first_item.created_at
