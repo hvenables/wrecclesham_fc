70dbde332cc48f0ac90f05cbec22e8561f5ac5ee
Harry Venables <venablesh@yahoo.co.uk>
2017-02-19 22:32:47 +0000

    Updated league table following changes to fixtures

--- app/models/league_table.rb
+++ app/models/league_table.rb
@@ -1,7 +1,7 @@
 class LeagueTable < ApplicationRecord
   has_many :seasons, dependent: :destroy
   has_many :teams
-  # has_many :fixtures
+  has_many :fixtures, as: :competition
 
   def self.active_first_team_table
     where(active_first_team_table: true).first
@@ -9,6 +9,14 @@ class LeagueTable < ApplicationRecord
 
   def self.active_reserve_team_table
     where(active_reserve_team_table: true).first
+  end
+
+  def scheduled_fixtures
+    fixtures.fixtures
+  end
+
+  def results
+    fixtures.results
   end
 
 end
