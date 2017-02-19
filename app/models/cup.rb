7c38ae8476b64c76ddb493a6adc5141a4a2b4d24
Harry Venables <venablesh@yahoo.co.uk>
2017-02-19 22:21:02 +0000

    Added fixtures to cups

--- app/models/cup.rb
+++ app/models/cup.rb
@@ -1,4 +1,16 @@
 class Cup < ApplicationRecord
   has_many :team_cups
   has_many :teams, through: :team_cups
+  has_many :fixtures, as: :competition
+
+  scope :active, -> { where(active: true) }
+
+  def scheduled_fixtures
+    fixtures.fixtures
+  end
+
+  def results
+    fixtures.results
+  end
+
 end
