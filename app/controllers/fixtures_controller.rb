98a3ca36610b5713a19de26604fb799ce123cdd9
Harry Venables <venablesh@yahoo.co.uk>
2017-02-19 22:31:05 +0000

    Updated the fixtures following addition of cups

--- app/controllers/fixtures_controller.rb
+++ app/controllers/fixtures_controller.rb
@@ -1,28 +1,33 @@
 class FixturesController < ApplicationController
+  before_action :load_team, only: [:create, :update]
+  before_action :load_subject, only: :show
 
-  def index
-    if params[:league_table_id]
-      @league_table = LeagueTable.find(params[:league_table_id])
-      @fixtures = Fixture.fixtures.league_fixtures(@league_table)
-      @results = Fixture.results.league_results(@league_table)
-    elsif params[:team_id]
-      @team = Team.find(params[:team_id])
-      @fixtures = Fixture.fixtures.team_fixtures(@team)
-      @results = Fixture.results.team_results(@team)
-    end
+  def show
+    @fixtures, @results = @subject.scheduled_fixtures, @subject.results
   end
 
   def create
-    team = Team.find(params[:team_id])
-    if params[:fixtures]
-      Fixture.create_fixtures(team)
-    else
-      Fixture.update_fixtures(team)
-    end
+    # Good to introduce if statement here
+    @team.create_fixtures
+    redirect_to team_fixtures_path(params[:team_id])
+  end
+
+  def update
+    @team.update_fixtures
     redirect_to team_fixtures_path(params[:team_id])
   end
 
   private
+
+  def load_team
+    @team = Team.find(params[:team_id]) if params[:team_id]
+  end
+
+  def load_subject
+    @subject = Team.find(params[:team_id]) if params[:team_id]
+    @subject ||= LeagueTable.find(params[:league_table_id]) if params[:league_table_id]
+    @subject ||= Cup.find(params[:cup_id]) if params[:cup_id]
+  end
 
   def fixture_params
     params.permit(:league_table_id)
