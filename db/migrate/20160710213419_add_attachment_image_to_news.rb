39ba2de04174d2297819bc6b8495c6524ce7d2fe
Harry Venables <venablesh@yahoo.co.uk>
2017-07-23 12:22:41 +0100

    Specfy rails version in migration

--- db/migrate/20160710213419_add_attachment_image_to_news.rb
+++ db/migrate/20160710213419_add_attachment_image_to_news.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddAttachmentImageToNews < ActiveRecord::Migration
+class AddAttachmentImageToNews < ActiveRecord::Migration[5.0]
   def self.up
     change_table :news do |t|
       t.attachment :image
