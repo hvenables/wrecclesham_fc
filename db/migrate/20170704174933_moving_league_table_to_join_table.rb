c4d9ae5ace6c5ab1c5e2218827e7f5338bd81c45
Harry Venables <venablesh@yahoo.co.uk>
2017-07-23 02:14:04 +0100

    Fix migrations

--- db/migrate/20170704174933_moving_league_table_to_join_table.rb
+++ db/migrate/20170704174933_moving_league_table_to_join_table.rb
@@ -2,11 +2,6 @@
 
 class MovingLeagueTableToJoinTable < ActiveRecord::Migration[5.0]
   def change
-    LeagueTable.all.each do |league_table|
-      league_table.teams << Team.where(league_table_id: league_table.id)
-      league_table.save
-    end
-
     remove_column :teams, :league_table_id
   end
 end
