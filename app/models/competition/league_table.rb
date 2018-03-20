4a0c358dd60bdb775a23fcb27c81e350ec705cc3
Harry Venables <venablesh@yahoo.co.uk>
2018-03-20 23:02:15 +0000

    Added specs for LeagueTable

--- app/models/competition/league_table.rb
+++ app/models/competition/league_table.rb
@@ -5,14 +5,6 @@ class Competition
     has_many :seasons, dependent: :destroy
     validates :name, :year, :url, :fixture_url, :result_url, presence: true
 
-    # validate :can_only_be_active_for_one_team
-    def can_only_be_active_for_one_team
-      return if active?
-      return unless active_first_team_table? || active_reserve_team_table?
-
-      errors.add(:league_table, 'can only be active for first team or reserve team')
-    end
-
     def self.active_first_team_table
       joins(:teams).find_by(active: true, teams: { name: 'Wrecclesham' })
     end
