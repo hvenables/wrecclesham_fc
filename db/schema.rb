c057fab9ea138305cf214f9b7a0aa8ebf588e35d
Harry Venables <venablesh@yahoo.co.uk>
2018-04-12 21:45:46 +0100

    fixup

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180323214027) do
+ActiveRecord::Schema.define(version: 20180412185658) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "abouts", id: :serial, force: :cascade do |t|
-    t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
