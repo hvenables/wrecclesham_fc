1e99226e7d4b340878c173e72d31dfc3f95ad3c4
Harry Venables <venablesh@yahoo.co.uk>
2018-09-01 10:43:34 +0100

    Added link to league table from fixtures

--- app/helpers/fixtures_helper.rb
+++ app/helpers/fixtures_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module FixturesHelper
+  def competition_path(competition)
+    case competition
+    when Competition::LeagueTable
+      league_table_path(competition)
+    when Competition::Cup
+      cup_fixtures_path(competition)
+    end
+  end
 end
